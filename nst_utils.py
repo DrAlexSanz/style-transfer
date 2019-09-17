9054d33101aa3711dcfe7e1080664702f7edc923
DrAlexSanz <52710912+DrAlexSanz@users.noreply.github.com>
2019-09-17 18:16:32 +0200

    Add files via upload

--- nst_utils.py
+++ nst_utils.py
@@ -16,7 +16,7 @@ class CONFIG:
     IMAGE_WIDTH = 400
     IMAGE_HEIGHT = 300
     COLOR_CHANNELS = 3
-    NOISE_RATIO = 0.6
+    NOISE_RATIO = 0.9
     MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
     VGG_MODEL = 'pretrained-model/imagenet-vgg-verydeep-19.mat' # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
     STYLE_IMAGE = 'images/stone_style.jpg' # Style image to use.
